47a489c075abb525df730e80a7b8fdd227e35057
itusharsingh <tushar@vibemonitor.ai>
2026-02-12 02:00:29 +0530

    add schema for analysis

---
+++ agents/code_analyzer/app/schema.py
@@ -0,0 +1,16 @@
+from pydantic import BaseModel
+from typing import List
+
+class FunctionSchema(BaseModel):
+    function_name: str
+    has_entry_log: bool
+    has_error_log: bool
+    has_try_except: bool
+    calls_external_io: bool
+
+class EachFile(BaseModel):
+    file_name:str
+    functions: list[FunctionSchema]
+
+
+
